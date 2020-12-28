ad70d764cde747da219b1c201722a04edb829409
Vinod.gentyal1609125629435 <vinod.gentyal@gmail.com>
2020-12-28 22:57:02 +0530

    Updates

---
+++ precache-manifest.3abda6eebd79dee005897acac4775a0c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "605858cc6ed453ad0651",
+    "url": "/Mini-E-commerce-app/static/css/main.687fccb3.chunk.css"
+  },
+  {
+    "revision": "605858cc6ed453ad0651",
+    "url": "/Mini-E-commerce-app/static/js/main.605858cc.chunk.js"
+  },
+  {
+    "revision": "1ed06b5e83eb21e629a1",
+    "url": "/Mini-E-commerce-app/static/js/1.1ed06b5e.chunk.js"
+  },
+  {
+    "revision": "eb6d2e4062bec5286821",
+    "url": "/Mini-E-commerce-app/static/js/runtime~main.eb6d2e40.js"
+  },
+  {
+    "revision": "d1679ffae59521c6297faba322466ce5",
+    "url": "/Mini-E-commerce-app/index.html"
+  }
+];
